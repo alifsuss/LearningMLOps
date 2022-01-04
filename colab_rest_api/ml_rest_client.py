c72157a2540dc6ce588ee33088d13e3e273f0ec6
Alif Sussardi <73863478+alifsuss@users.noreply.github.com>
2022-01-04 14:30:34 +0700

    add script to deploy PyTorch model

--- colab_rest_api/ml_rest_client.py
+++ colab_rest_api/ml_rest_client.py
@@ -1,11 +1,8 @@
 import json
 import requests
 
-url = 'http://da203f8e224e.ngrok.io/predict'
+url = 'http://0b21-34-125-164-19.ngrok.io/predict'
 
 request_data = json.dumps({'age':40,'salary':20000})
 response = requests.post(url,request_data)
 print (response.text)
-
-
-
